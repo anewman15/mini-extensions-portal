43839cc86256638fcbe5f953ea5816c9064f7a34
Abdullah Numan <anewman15@hotmail.com>
2021-10-22 03:10:50 +0600

    Render Login comp if user not availabe in store

--- src/routes/PortalRoute.tsx
+++ src/routes/PortalRoute.tsx
@@ -1,24 +1,34 @@
 import React, { ReactElement } from 'react';
+import { useSelector } from 'react-redux';
 import { Route } from 'react-router';
-import { Record, FieldSet } from 'airtable';
+import Login from '../components/Login';
 import { Portal } from '../data/PortalsData';
+import PortalPage from '../pages/PortalPage';
+import { StoreStateType } from '../redux/store/store';
 
 type PortalRouteProps = {
   exact: boolean,
   path: string,
-  user: Record<FieldSet> | {},
-  component: any,
   portalObject: Portal,
 };
 
-const PortalRoute = ({ user, component: Component, ...others }: PortalRouteProps): ReactElement => (
-  <Route
-    {...others}
-    render={(props: any) => {
-      return <Component {...others} {...props} />;
+const PortalRoute = (portalProps: PortalRouteProps): ReactElement => {
+  const user = useSelector((state: StoreStateType) => state.user);
+  console.log(user);
+  return (
+    <Route
+      {...portalProps}
+      render={
+          (props: any) => {
+          if (Object.keys(user).length) {
+            return <PortalPage {...portalProps} {...props} />;
+          } else {
+            return <Login />
+          }
+        }
       }
-    }
-  />
-);
+    />
+  )
+};
 
 export default PortalRoute

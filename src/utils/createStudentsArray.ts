a99c4b68ba88f51b279c436d178ebcac3eb4193e
Abdullah Numan <anewman15@hotmail.com>
2021-10-24 00:42:55 +0600

    Rename createStudentsArray to createLinkedRecordsIdsArray, modify method def with portal

--- src/utils/createStudentsArray.ts
+++
@@ -1,9 +0,0 @@
-import { Record, Records, FieldSet } from "airtable";
-
-const createStudentsArray = (records: Records<FieldSet>) => {
-  const studentIdArray: any[] = [];
-  records.forEach((record: Record<FieldSet>) => studentIdArray.push(record.fields.Students));
-  return studentIdArray.flat();
-};
-
-export default createStudentsArray;

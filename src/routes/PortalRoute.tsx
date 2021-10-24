4a9354fab03b66bb1090e73953faeb85fa1f8d62
Abdullah Numan <anewman15@hotmail.com>
2021-10-25 04:29:31 +0600

    Remove user check from PortalRoute

--- src/routes/PortalRoute.tsx
+++ src/routes/PortalRoute.tsx
@@ -1,10 +1,7 @@
 import React, { ReactElement } from 'react';
-import { useSelector } from 'react-redux';
 import { Route } from 'react-router';
-import Login from '../components/Login';
 import { Portal } from '../data/PortalsData';
 import PortalPage from '../pages/PortalPage';
-import { StoreStateType } from '../redux/store/store';
 
 type PortalRouteProps = {
   exact: boolean,
@@ -13,19 +10,12 @@ type PortalRouteProps = {
 };
 
 const PortalRoute = (portalProps: PortalRouteProps): ReactElement => {
-  const user = useSelector((state: StoreStateType) => state.user);
   return (
     <Route
       {...portalProps}
       render={
           (props: any) => {
-          if (Object.keys(user).length) {
             return <PortalPage {...portalProps} {...props} />;
-          } else {
-            const { portalObject } = portalProps;
-            console.log(portalObject.loginFields);
-            return <Login portal={portalObject} {...props} />;
-          }
         }
       }
     />

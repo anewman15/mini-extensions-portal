9e565114ea9e090a5d7606ae8da18d74e581f34d
Abdullah Numan <anewman15@hotmail.com>
2021-10-25 11:45:43 +0600

    Style Landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,14 +4,17 @@ import { Portal, portals } from '../data/PortalsData';
 
 const Landing = () => {
   const portalLinks = portals.map((portal: Portal) => (
-    <div key={`${portal.id}`}>
-      <Link to={`/portals/${portal.id}`}>{portal.id}</Link>
-    </div>
+    <h3 key={`${portal.id}`} className="text-l font-bold my-3">
+      <Link className="text-gray-600 hover:text-gray-800" to={`/portals/${portal.id}`}>{portal.id}</Link>
+    </h3>
   ));
 
   return (
-    <div>
-      {portalLinks}
+    <div className="container mx-auto my-8 w-10/12">
+      <h2 className="my-3 p-4 text-green-800 text-xl text-center font-semibold">Your Portals</h2>
+      <div className="container my-3 mx-auto py-4 px-8 w-8/12 bg-gray-100 rounded filter drop-shadow">
+        {portalLinks}
+      </div>
     </div>
   )
 };
